183bde164ad7562b953ede93764154e700bbb41c
chinmaimore <102403078+chinmaimore@users.noreply.github.com>
2022-03-26 11:37:15 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+struct student
+{
+	char usn;
+	char student name;
+	int sem
+	float m1,m2,m3,m4,m5,average;
+};
+	struct student s[10];
+	int i,n;
+	printf("enter no of student records);
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+}
+	printf("student usn");
+	scanf("%S",s[i].usn);
+	printf("students name");
+	scanf("%s",s[i].student name);
+	printf("enter semester");
+	scanf("%d",s[i].sem);
+	printf("enter 5 subjects marks");
+	scanf("%f%f%f%f%f",&s[i]m1,&s[i]m2,&s[i]m3,&s[i]m4,&s[i]m5);
+}
+	for(i=0;i<n;i++)
+}
+	s[i]average=(s[i]m1
+
+
+
